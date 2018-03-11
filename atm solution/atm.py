368886bbb1ff62ba635bdf05fbbfecc4ff8f2d19
made-man-12 <made_man_12@live.com>
2018-03-11 23:52:30 +0200

    fix2 atm

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -2,34 +2,34 @@ user_amount = input('Please enter Amount: ')
 user_balance = input('Please enter Balance: ')
 amount = user_amount
 if user_balance > user_amount and user_balance > 0 and user_amount > 0 :
-    while amount >= 100:
-        print 'give 100'
-        amount = amount - 100
-    while amount >= 50:
-        print 'give 50'
-        amount = amount - 50
+    while amount > 0 :
+        if amount >= 100:
+            print 'give 100'
+            amount = amount - 100
+        elif amount >= 50:
+            print 'give 50'
+            amount = amount - 50
 
-    while amount >= 10:
-        print 'give 10'
-        amount = amount - 10
+        elif amount >= 10:
+            print 'give 10'
+            amount = amount - 10
 
-    while amount >= 5:
-        print 'give 5'
-        amount = amount - 5
+        elif amount >= 5:
+            print 'give 5'
+            amount = amount - 5
 
-    while amount >= 2:
-        print 'give 2'
-        amount = amount - 2
-
-    if amount == 1:
-        print 'This is the remaining:', amount
-        total = user_balance - user_amount + amount
-        print 'Balance remaining', total
-
-    else:
-        print 'The full amount has been withdraw'
-        total = user_balance - user_amount
-        print 'Balance remaining', total
+        elif amount >= 2:
+            print 'give 2'
+            amount = amount - 2
+        elif amount == 1:
+            print 'This is the remaining:', amount
+            total = user_balance - user_amount + amount
+            print 'Balance remaining', total
+            amount = amount - 1
+        else:
+            print 'The full amount has been withdraw'
+            total = user_balance - user_amount
+            print 'Balance remaining', total
 
 else:
     if user_balance > 0 or user_amount > 0 :

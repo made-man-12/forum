c2d8db0dd82774af51d971d98a605bcc1681d64c
made-man-12 <made_man_12@live.com>
2018-03-11 19:58:45 +0200

    atm solution

---
+++ atm solution/atm.py
@@ -0,0 +1,22 @@
+money = 500
+
+request = 277
+
+while request >= 100:
+    print 'give 100'
+    request = request - 100
+while request >= 50:
+    print 'give 50'
+    request = request - 50
+
+while request >= 10:
+    print 'give 10'
+    request = request - 10
+
+while request >= 5:
+    print 'give 5'
+    request = request - 5
+
+while request >= 2:
+    print 'give 2'
+    request = request - 2

e3ee84ded8fa0573206536d2ebb94ed6dbc2d648
made-man-12 <made_man_12@live.com>
2018-03-15 23:17:02 +0200

    Fix3 function

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,52 +1,52 @@
 """ Function section"""
-def withdraw(user_balance):
-    if len(user_balance) > 0 and user_balance.isdigit() is True and int(user_balance) > 0:
-        balance = int(user_balance)
-        while (True):
-            user_amount = raw_input('Please enter Amount (q for quit): ')
-            print 'Current', balance
-            if user_amount != 'q':
-                if len(user_amount) > 0 and user_amount.isdigit() is True and  int(user_amount) > 0 :
-                    amount = user_amount
+def withdraw(balance, amount):
+    amount_discount = amount
 
-                    if balance < int(amount):
-                        print "Can't give you all this money !!"
-                        break;
-                    else:
-                        amount_discount = int(amount)
+    while amount_discount > 0:
+        if amount_discount >= 100:
+            print 'give 100'
+            amount_discount = amount_discount - 100
+        elif amount_discount >= 50:
+            print 'give 50'
+            amount_discount = amount_discount - 50
 
-                        while amount_discount > 0:
-                            if amount_discount >= 100:
-                                print 'give 100'
-                                amount_discount = amount_discount - 100
-                            elif amount_discount >= 50:
-                                print 'give 50'
-                                amount_discount = amount_discount - 50
+        elif amount >= 10:
+            print 'give 10'
+            amount_discount = amount_discount - 10
 
-                            elif amount >= 10:
-                                print 'give 10'
-                                amount_discount = amount_discount - 10
+        elif amount_discount >= 5:
+            print 'give 5'
+            amount_discount = amount_discount - 5
 
-                            elif amount_discount >= 5:
-                                print 'give 5'
-                                amount_discount = amount_discount - 5
+        else:
+            print 'give', amount_discount
+            amount_discount = 0
 
-                            else:
-                                print 'give', amount_discount
-                                amount_discount = 0
-
-                        balance = balance - int(amount)
-
-                else:
-                    print 'Please check the Amount'
-                    break;
-            else:
-                print 'Thank you for use ATM'
-                break;
-    else:
-        print 'Please check the Balance'
+    return balance - amount
 
 
 """App Section"""
 user_balance = raw_input('Please enter Balance: ')
-withdraw(user_balance)
+if len(user_balance) > 0 and user_balance.isdigit() is True and int(user_balance) > 0:
+    balance = int(user_balance)
+    while (True):
+        user_amount = raw_input('Please enter Amount (q for quit): ')
+        print 'Current', balance
+        if user_amount != 'q':
+            if len(user_amount) > 0 and user_amount.isdigit() is True and int(user_amount) > 0:
+                amount = int(user_amount)
+
+                if balance < amount:
+                    print "Can't give you all this money !!"
+                    break;
+                else:
+                    balance = withdraw(balance,amount)
+            else:
+                print 'Please check the Amount'
+                break;
+        else:
+            print 'Thank you for use ATM'
+            break;
+else:
+    print 'Please check the Balance'
+

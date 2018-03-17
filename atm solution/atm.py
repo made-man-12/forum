62d3a026f96dc321fe39e2f1be06b594805afa1d
made-man-12 <made_man_12@live.com>
2018-03-18 01:56:15 +0200

    Turn all app to class

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,53 +1,71 @@
+class ATM:
 
-""" Function section"""
-def withdraw(balance, amount):
-    amount_discount = amount
+    def __init__(self):
+        self.withdraw()
 
-    while amount_discount > 0:
-        if amount_discount >= 100:
-            print 'give 100'
-            amount_discount = amount_discount - 100
-        elif amount_discount >= 50:
-            print 'give 50'
-            amount_discount = amount_discount - 50
 
-        elif amount >= 10:
-            print 'give 10'
-            amount_discount = amount_discount - 10
+    def withdraw (self):
+        user_balance = raw_input('Please enter Balance: ')
+        user_bank_name = raw_input("Enter The bank name:")
 
-        elif amount_discount >= 5:
-            print 'give 5'
-            amount_discount = amount_discount - 5
-
+        if self.v_input(user_balance, True) is True and self.v_input(user_bank_name, False) is True :
+            self.balance = int(user_balance)
+            bank_name = user_bank_name
         else:
-            print 'give', amount_discount
-            amount_discount = 0
+            print 'Please check the Balance Or Bank Name'
+            return
 
-    return balance - amount
+        while (True):
+            user_amount = raw_input('Please enter Amount (q for quit): ')
 
+            self.dash(30)
+            self.dash(30)
+            print 'Welcome to %s' %bank_name
+            print 'Current Balance = %d' %self.balance
+            self.dash(30)
 
-"""App Section"""
-user_balance = raw_input('Please enter Balance: ')
-if len(user_balance) > 0 and user_balance.isdigit() is True and int(user_balance) > 0:
-    balance = int(user_balance)
-    while (True):
-        user_amount = raw_input('Please enter Amount (q for quit): ')
-        print 'Current', balance
-        if user_amount != 'q':
-            if len(user_amount) > 0 and user_amount.isdigit() is True and int(user_amount) > 0:
-                amount = int(user_amount)
-
-                if balance < amount:
-                    print "Can't give you all this money !!"
-                    break
+            if user_amount != 'q':
+                if self.v_input(user_amount, True) is True :
+                    amount = int(user_amount)
+                    if self.balance < amount:
+                        print "Can't give you all this money !!"
+                        break;
+                    else:
+                        self.balance = self.rebate_money(amount)
                 else:
-                    balance = withdraw(balance,amount)
+                    print 'Please check the Amount'
+                    break;
             else:
-                print 'Please check the Amount'
-                break
-        else:
-            print 'Thank you for use ATM'
-            break
-else:
-    print 'Please check the Balance'
+                print 'Thank you for use ATM'
+                break;
+            self.dash(45)
 
+
+    def v_input(self, item, type = True):
+        if type is True:
+            if len(item) > 0 and item.isdigit() is True and int(item) > 0:
+                return True
+        else:
+            if len(item) > 0 and item.isdigit() is False:
+                return True
+
+
+    def dash (self, time = 40):
+        print '-' * time
+
+
+    def rebate_money(self, amount):
+        rebate = amount
+        while rebate > 0:
+            for bnote in [100,50,10,5,1]:
+                while rebate >= bnote:
+                    if bnote >= 5:
+                        print 'give %d' %bnote
+                        rebate = rebate - bnote
+                    else:
+                        print 'give %d' %rebate
+                        rebate = 0
+
+        self.balance -= amount
+
+        return self.balance

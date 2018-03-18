70b9df2871d9c3b8ed04007264fafaf11a0810cb
made-man-12 <made_man_12@live.com>
2018-03-19 00:17:49 +0200

    Create Entities

---
+++ Forums/main.py
@@ -0,0 +1,13 @@
+from models import Member, Post
+
+
+Member('member1', 50)
+Member('member2', 40)
+
+Post('post1', 'content1')
+Post('post2', 'content2')
+Post('post3', 'content3')
+
+
+
+
---
+++ Forums/models.py
@@ -0,0 +1,16 @@
+
+class   Member:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+    def __repr__(self):
+        return "Name: %s Age: %d" %(self.name, self.age)
+
+class Post():
+    def __init__(self, title, content):
+        self.title = title
+        self.content = content
+
+    def __repr__(self):
+        return "Title: %s Content: %s" %(self.title, self.content)

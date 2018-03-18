fd5143f5b935eeb9067a583a323b4df58dc5437d
made-man-12 <made_man_12@live.com>
2018-03-19 01:11:15 +0200

    add store

--- Forums/main.py
+++ Forums/main.py
@@ -1,12 +1,26 @@
 from models import Member, Post
+from store  import MemberStore, PostStore
+
+m_1 = Member('member1', 50)
+m_2 = Member('member2', 40)
+
+p_1 = Post('post1', 'content1')
+p_2 = Post('post2', 'content2')
+p_3 = Post('post3', 'content3')
+
+m_s = MemberStore()
+p_s = PostStore()
+
+m_s.add(m_1)
+m_s.add(m_2)
+
+p_s.add(p_1)
+p_s.add(p_2)
+p_s.add(p_3)
 
 
-Member('member1', 50)
-Member('member2', 40)
-
-Post('post1', 'content1')
-Post('post2', 'content2')
-Post('post3', 'content3')
+print m_s.get_all()
+print p_s.get_all()
 
 
 
---
+++ Forums/store.py
@@ -0,0 +1,20 @@
+class MemberStore:
+
+    members = []
+
+    def get_all(self):
+        return MemberStore.members
+
+    def add(self, member):
+        MemberStore.members.append(member)
+
+
+class PostStore :
+    posts = []
+
+    def get_all(self):
+        return PostStore.posts
+
+    def add(self, post):
+        PostStore.posts.append(post)
+

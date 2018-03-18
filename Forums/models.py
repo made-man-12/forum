c32bca1aac337a24d71a67f8a4e50c62ebd54978
made-man-12 <made_man_12@live.com>
2018-03-19 00:19:37 +0200

    delete file

--- Forums/models.py
+++ Forums/models.py
@@ -3,7 +3,7 @@ class   Member:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        
+
     def __repr__(self):
         return "Name: %s Age: %d" %(self.name, self.age)
 

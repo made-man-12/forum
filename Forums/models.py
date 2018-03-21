03f464e05940e409873e444e571880b0fb8942e5
made-man-12 <made_man_12@live.com>
2018-03-22 00:08:30 +0200

    filtter p-2

--- Forums/models.py
+++ Forums/models.py
@@ -4,6 +4,7 @@ class Member:
         self.id = 0
         self.name = name
         self.age = age
+        self.posts = []
 
     def __str__(self):
         return "Name: %s Age: %d" %(self.name, self.age)
@@ -12,10 +13,11 @@ class Member:
         return self.name != other.name and self.age != other.age and self.id != other.id
 
 class Post:
-    def __init__(self, title, content):
+    def __init__(self, title, content,member_id=0):
         self.id = 0
         self.title = title
         self.content = content
+        self.member_id = member_id
 
     def __str__(self):
         return "Title: %s Content: %s" %(self.title, self.content)
--- Forums/stores.py
+++ Forums/stores.py
@@ -37,16 +37,30 @@ class MemberStore:
             if new_member.id is member.id:
                 self.delete(member.id)
                 MemberStore.members.append(new_member)
-                MemberStore.members.sort(key=lambda members: members.id)
+                MemberStore.members.sort(key=lambda members: member.id)
 
     def get_by_name(self, member_name):
         all_members = self.get_all()
         result = []
         for member in all_members:
             if member_name is member.name:
-                result.append(str(member))
+                 result.append(str(member))
         return result
 
+    def get_members_with_posts(self, all_posts):
+        all_members = self.get_all()
+
+        for post in all_posts:
+            for member in all_members:
+                if post.member_id == member.id:
+                    if post not in member.posts:
+                        member.posts.append(post)
+        return all_members
+
+    def get_top_two(self, all_posts):
+        all_members_posts = self.get_members_with_posts(all_posts)
+        all_members_posts.sort(key=lambda member: len(member.posts), reverse=True)
+        return all_members_posts
 
 class PostStore :
     posts = []
@@ -85,4 +99,5 @@ class PostStore :
         for post in all_post:
             if new_post.id is post.id:
                 self.delete(post.id)
-                PostStore.members.append(new_post)
+                PostStore.post.append(new_post)
+                PostStore.post.sort(key=lambda members: member.id)
